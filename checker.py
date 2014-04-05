7472db5b6dc9ef97e1d11353f82cbefda3e4809d
Maxwell Forbes <m.b.forbes@gmail.com>
2014-04-04 22:49:46 -0700

    Several features to make usable: - email reports vs printing now specifiable via command line - help now possible with -h,--help - can specify any directory to check, with default to ~ (instead of my home hardcoded) - expands ~ correctly

--- checker.py
+++ checker.py
@@ -17,39 +17,30 @@ __author__ = 'mbforbes'
 import subprocess as sp
 import os
 import smtplib
+import sys
 from email.mime.text import MIMEText
 from smtplib import SMTP_SSL as SMTP
 
 #
 # CONSTANTS
 #
-
+CMDLINE_HELP = ['-h', '--h']
+CMDLINE_REPORT = ['--email', '--print', '--both']
 #
 # CLASSES
 #
-
 
 #
 # FUNCTIONS
 #
 
         
-#
-# MAIN
-#
-
-def main():
+def main(checkdir='~', report='--both'):
     '''Check git directories for uncommited files and unpushed commits. Then
     send an email report to the user.'''
     # Settings.
-    check_dir = '/Users/max'
-    # Who to send report to.
-    with open('recipient') as recipient:
-        user_email = recipient.read().strip()
-
-    # Account with which to send the email from
-    with open('sender') as sender:
-        sender_uname, sender_psswd = sender.read().strip().split('\n')
+    check_dir = os.path.expanduser(checkdir)
+    print 'Checking', check_dir, '...'
 
     # Get the void.
     dn = open(os.devnull, 'w')
@@ -60,7 +51,8 @@ def main():
         '-name', # search by name
         '.git'], # the name we want
         stdout=sp.PIPE, # catch output
-        stderr=dn) # send errors to the void!
+    #    stderr=dn # send errors to the void!
+        ) 
     res, err = p.communicate()
 
     # close the void
@@ -98,14 +90,35 @@ def main():
     # append any dirty directories
     if len(dirtydirs) > 0:
         msgstr += 'The following directories have dirty WDs:\n' + \
-            '\n'.join(['\t - ' + x for x in dirtydirs]) + '\n\n'
+            '\n'.join(['\t - ' + x for x in dirtydirs])
+    if len(dirtydirs) > 0 and len(unpusheddirs) > 0:
+        msgstr += '\n'
     if len(unpusheddirs) > 0:
         msgstr += 'The following directories need to be pushed:\n' + \
         '\n'.join(['\t - ' + x for x in unpusheddirs])
-    msg = MIMEText(msgstr)
 
-    # debug
-    #print msgstr
+    # It's not useful unless you tell someone about it!
+    if report == '--print' or report == '--both':
+        print_report(msgstr)
+    if report == '--email' or report == '--both':
+        email_report(msgstr)
+
+def print_report(msgstr):
+    '''Print report to console.'''
+    print msgstr
+
+def email_report(msgstr):
+    '''Send email report.'''
+    # Who to send report to.
+    with open('recipient') as recipient:
+        user_email = recipient.read().strip()
+
+    # Account with which to send the email from
+    with open('sender') as sender:
+        sender_uname, sender_psswd = sender.read().strip().split('\n')
+
+    # Convert format.
+    msg = MIMEText(msgstr)
 
     # Set email fields.
     receiver = user_email
@@ -121,6 +134,53 @@ def main():
     finally:
         conn.close()
 
+def usage():
+    '''Tell how to use this program.'''
+    print 'git-checker : Your friendly neighborhood git repository checker.'
+    print '              Finds dirty / unpushed repositories and tells you.'
+    print
+    print 'Usage: python checker.py [option] [report_option]'
+    print
+    print '[option] is one of:'
+    print ', '.join(CMDLINE_HELP), '        Display this help message and exit.'
+    print '</some/path>    Attempt to use the string as a path for checking.'
+    print '                Whitespace sensitive (use quotes), defaults to ~.'
+    print
+    print '[report_option] is one of:'
+    print '--print         Prints report to console (not printer).'
+    print '--email         Emails report using sender and recipient files.'
+    print '--both          Default. Print and email.'
+    print
 
 if __name__ == '__main__':
-    main()
+    # Defaults!
+    checkdir = '~'
+    report = '--both'
+
+    # At least one option!
+    if len(sys.argv) > 1:
+        arg1 = sys.argv[1]
+        # Could be help
+        if arg1 in CMDLINE_HELP:
+            usage()
+            exit(0)
+        # Could be cmdline report option.
+        elif arg1 in CMDLINE_REPORT:
+            report = arg1
+        # Otherwise, assume it's a path.
+        else:
+            checkdir = arg1
+
+    # At least two options!
+    if len(sys.argv) > 2:
+        arg2 = sys.argv[2]
+        # There's no reason this should be the path (or help), so check only 
+        # report.
+        if arg2 in CMDLINE_REPORT:
+            report = arg2
+
+    # Actually do things!
+    main(checkdir, report)
+
+
+
